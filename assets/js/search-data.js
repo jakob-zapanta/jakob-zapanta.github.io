b1111e17d5123f9f941ba209c0260825b03efecd
jakob-zapanta <jakob.zapanta@tufts.edu>
2025-12-31 06:05:48 +0000

    Deploying to gh-pages from @ jakob-zapanta/jakob-zapanta.github.io@2cfe45b39d758ffc9e5cd8048f3d3ee852016eca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,21 +7,21 @@ ninja.data = [{
     title: "about",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/";
+      window.location.href = "/jakob-zapanta.github.io/";
     },
   },{id: "nav-projects",
           title: "projects",
           description: "My ever-growing collection of cool projects",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/jakob-zapanta.github.io/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/jakob-zapanta.github.io/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
@@ -31,7 +31,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2025/plotly/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2025/plotly/";
           
         },
       },{id: "post-a-post-with-image-galleries",
@@ -42,7 +42,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/photo-gallery/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/photo-gallery/";
           
         },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
@@ -64,7 +64,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/tabs/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/tabs/";
           
         },
       },{id: "post-a-post-with-typograms",
@@ -75,7 +75,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/typograms/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/typograms/";
           
         },
       },{id: "post-a-post-that-can-be-cited",
@@ -86,7 +86,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/post-citation/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/post-citation/";
           
         },
       },{id: "post-a-post-with-pseudo-code",
@@ -97,7 +97,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/pseudocode/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/pseudocode/";
           
         },
       },{id: "post-a-post-with-code-diff",
@@ -108,7 +108,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/code-diff/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/code-diff/";
           
         },
       },{id: "post-a-post-with-advanced-image-components",
@@ -119,7 +119,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/advanced-images/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/advanced-images/";
           
         },
       },{id: "post-a-post-with-vega-lite",
@@ -130,7 +130,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/vega-lite/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/vega-lite/";
           
         },
       },{id: "post-a-post-with-geojson",
@@ -141,7 +141,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/geojson-map/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/geojson-map/";
           
         },
       },{id: "post-a-post-with-echarts",
@@ -152,7 +152,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/echarts/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/echarts/";
           
         },
       },{id: "post-a-post-with-chart-js",
@@ -163,7 +163,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2024/chartjs/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2024/chartjs/";
           
         },
       },{id: "post-a-post-with-tikzjax",
@@ -174,7 +174,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/tikzjax/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/tikzjax/";
           
         },
       },{id: "post-a-post-with-bibliography",
@@ -185,7 +185,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/post-bibliography/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/post-bibliography/";
           
         },
       },{id: "post-a-post-with-jupyter-notebook",
@@ -196,7 +196,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/jupyter-notebook/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/jupyter-notebook/";
           
         },
       },{id: "post-a-post-with-custom-blockquotes",
@@ -207,7 +207,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/custom-blockquotes/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/custom-blockquotes/";
           
         },
       },{id: "post-a-post-with-table-of-contents-on-a-sidebar",
@@ -218,7 +218,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/sidebar-table-of-contents/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/sidebar-table-of-contents/";
           
         },
       },{id: "post-a-post-with-audios",
@@ -229,7 +229,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/audios/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/audios/";
           
         },
       },{id: "post-a-post-with-videos",
@@ -240,7 +240,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/videos/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/videos/";
           
         },
       },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
@@ -251,7 +251,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/tables/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/tables/";
           
         },
       },{id: "post-a-post-with-table-of-contents",
@@ -262,7 +262,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2023/table-of-contents/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2023/table-of-contents/";
           
         },
       },{id: "post-a-post-with-giscus-comments",
@@ -273,7 +273,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2022/giscus-comments/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2022/giscus-comments/";
           
         },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
@@ -295,7 +295,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/jakob-zapanta.github.io/assets/pdf/example_pdf.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
@@ -306,7 +306,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2021/diagrams/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2021/diagrams/";
           
         },
       },{id: "post-a-distill-style-blog-post",
@@ -317,7 +317,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2021/distill/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2021/distill/";
           
         },
       },{id: "post-a-post-with-twitter",
@@ -328,7 +328,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2020/twitter/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2020/twitter/";
           
         },
       },{id: "post-a-post-with-disqus-comments",
@@ -339,7 +339,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/disqus-comments/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2015/disqus-comments/";
           
         },
       },{id: "post-a-post-with-math",
@@ -350,7 +350,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/math/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2015/math/";
           
         },
       },{id: "post-a-post-with-code",
@@ -361,7 +361,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/code/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2015/code/";
           
         },
       },{id: "post-a-post-with-images",
@@ -372,7 +372,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/images/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2015/images/";
           
         },
       },{id: "post-a-post-with-formatting-and-links",
@@ -383,14 +383,14 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/blog/2015/formatting-and-links/";
+            window.location.href = "/jakob-zapanta.github.io/blog/2015/formatting-and-links/";
           
         },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
+              window.location.href = "/jakob-zapanta.github.io/books/the_godfather/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -398,7 +398,7 @@ ninja.data = [{
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/jakob-zapanta.github.io/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
@@ -406,47 +406,47 @@ ninja.data = [{
           title: 'M.A.T.E.R.I.A.L.S. (&amp;amp; Manufacturing)',
           description: "Metal And Textile Exoskeletal Reinforcement Incorporating Aluminum &amp; Leather Sheets",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/1_project/";
             },},{id: "projects-truss-design-construction-amp-analysis",
           title: 'Truss design, construction, &amp;amp; analysis',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/3_project/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/4_project/";
             },},{id: "projects-functional-3d-prints",
           title: 'Functional 3D Prints',
           description: "it&#39;s not a dog, but...",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/6_project/";
             },},{id: "projects-3d-printing-my-dawg",
           title: '3D Printing my Dawg',
           description: "and other artsy prints",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/7_project/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/8_project/";
             },},{id: "projects-assessing-the-correlation-between-stellar-flares-and-spots",
           title: 'Assessing the correlation between stellar flares and spots',
           description: "Astrophysics Data Analysis",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/jakob-zapanta.github.io/projects/9_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
