60814602d0f115650a2ef6d27fe4578118b89d68
jakob-zapanta <jakob.zapanta@tufts.edu>
2026-01-06 02:07:30 +0000

    Deploying to gh-pages from @ jakob-zapanta/jakob-zapanta.github.io@7c1d559085d631ea44f7f7836de8ad1f8e4efcbc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,13 +445,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/Astro_code/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
